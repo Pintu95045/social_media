85fa27a0d4446ae6d417ae7f7a23109f67de4657
Pintu Giri <mrpintugiri@gmail.com>
2025-03-05 18:32:42 +0530

    Fixed HashRouter issue for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { HashRouter } from 'react-router-dom';  // ✅ Add this
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <HashRouter>  {/* ✅ Wrap inside HashRouter */}
+      <App />
+    </HashRouter>
   </Provider>
 );
